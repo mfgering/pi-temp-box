2eb5642629eb79d1950dcc2e013afee66cb1e897
Mike Gering <mike@mfg-consulting-llc.com>
2021-12-20 14:10:32 -0500

    json

--- node-app/routes/server.js
+++ node-app/routes/server.js
@@ -1,10 +1,32 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/sensor', function(req, res, next) {
+/* GET json sensor page. */
+router.get('/', function(req, res, next) {
+  let json = get_sensor_data();
+  temp = json.temp;
+  humidity = json.humidity;
+  res.render('sensor', { title: 'Sensor', temp: temp, humidity: humidity });
+});
 
-  res.render('index', { title: 'Express' });
+/* GET json sensor page. */
+router.get('/json', function(req, res, next) {
+  let json = get_sensor_data();
+  temp = json.temp;
+  humidity = json.humidity;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(json))
 });
 
 module.exports = router;
+
+function get_sensor_data() {
+  const command = '/home/pi/projects/pi-temp-box/pi_temp/get_sensor.py';
+  var temp = -1;
+  var humidity = -1;
+  const child = require('child_process');
+  var data = child.execSync(command)
+  let json_v = JSON.parse(data);
+  return json_v;
+}
